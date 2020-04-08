88ef067362184ba2bc971eea29cefb43cb2b4dde
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-04-08 16:52:58 -0400

    Step 1: Ensure dice images follow a naming convention

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 // A random Number between 1 and 6, inclusive
 let roll = Math.floor(Math.random() * 6) + 1
 
+/* Approach: */
+
+// 1. Ensure the images follow a naming convention
+
 console.log(roll);  // 1, 2, 3, 4, 5, or 6
